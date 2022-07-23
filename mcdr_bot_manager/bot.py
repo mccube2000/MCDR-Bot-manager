528569de4a8053e912670b003dd26ca288919fa3
MC_cubes <mccube2000@outlook.com>
2022-07-23 11:05:14 +0800

    update

--- mcdr_bot_manager/bot.py
+++ mcdr_bot_manager/bot.py
@@ -24,9 +24,10 @@ class Bot:
         self.info = Botinfo
         self.qtype = qtype
         #spawm
-        server.execute(
-            f'execute at {info.player} run player bot_{self.info.name} spawn{self.spawn_argument()}'
-        )
+        # server.execute(
+        #     f'execute at {info.player} run player bot_{self.info.name} spawn{self.spawn_argument()}'
+        # )
+        server.execute(f'player bot_{self.info.name} spawn{self.spawn_argument()}')
 
     def kill(self, server: PluginServerInterface):
         #kill
